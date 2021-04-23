0d2d7d110475ad0d7291a5f94f7b9dca8f9b39c8
Basique Evangelist <basiqueevangelist@yandex.ru>
2021-04-23 13:28:11 +0300

    Show links to users when mentioned in view

--- PBug/Utils/RenderHelpers.cs
+++ PBug/Utils/RenderHelpers.cs
@@ -6,7 +6,7 @@ namespace PBug.Utils
 {
     public static class RenderHelpers
     {
-        public static HtmlString ShowUser(User user)
+        public static HtmlString ShowUser(User user, bool withLink = true)
         {
             StringBuilder builder = new();
             if (user == null)
@@ -15,10 +15,18 @@ namespace PBug.Utils
             }
             else
             {
+                if (withLink)
+                {
+                    builder.Append("<a class=\"userlink\" href=\"/user/");
+                    builder.Append(user.Username);
+                    builder.Append("\">");
+                }
                 builder.Append(user.FullName);
                 builder.Append(" <span class=\"username\">@");
                 builder.Append(user.Username);
                 builder.Append("</span>");
+                if (withLink)
+                    builder.Append("</a>");
             }
             return new HtmlString(builder.ToString());
         }
